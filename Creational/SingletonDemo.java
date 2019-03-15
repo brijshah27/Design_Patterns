53c2da4b9c0a5eb152df9388787737c6f370e629
brijshah27 <brijshah27@gmail.com>
2019-03-14 19:54:24 -0500

    Create singleton class

---
+++ Creational/SingletonDemo.java
@@ -0,0 +1,15 @@
+package Creational;
+
+public class SingletonDemo {
+
+    public static void main(String[] args) {
+
+        Singleton instance1 = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+
+        System.out.println(instance1);
+        System.out.println(instance2);
+
+        System.out.println(instance1 == instance2);
+    }
+}
