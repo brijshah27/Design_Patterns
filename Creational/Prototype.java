a11c5405b52b9a70be377bbe020438eb891ade9c
brijshah27 <brijshah27@gmail.com>
2019-03-20 22:03:11 -0500

    prototype pattern

---
+++ Creational/Prototype.java
@@ -0,0 +1,16 @@
+package Creational;
+
+public class Prototype implements Cloneable {
+
+    public Object clone() {
+        Object clone = null;
+
+        try {
+            clone = super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        return clone;
+    }
+}
---
+++ Creational/PrototypeDemo.java
@@ -0,0 +1,11 @@
+package Creational;
+
+public class PrototypeDemo {
+
+    public static void main(String[] args){
+        Prototype object1 = new Prototype();
+        System.out.println(object1);
+        Prototype object2 = (Prototype) object1.clone();
+        System.out.println(object2);
+    }
+}
