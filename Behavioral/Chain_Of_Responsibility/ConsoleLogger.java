60a4cb6a79e722e7a4742b4227a343ee039592a1
brijshah27 <brijshah27@gmail.com>
2019-03-26 20:21:06 -0500

    Chain of Responsibility

---
+++ Behavioral/Chain_Of_Responsibility/ConsoleLogger.java
@@ -0,0 +1,12 @@
+package Behavioral.Chain_Of_Responsibility;
+
+public class ConsoleLogger extends AbstractLogger {
+    public ConsoleLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Standard Console::Logger: " + message);
+    }
+}
---
+++ Behavioral/Chain_Of_Responsibility/FileLogger.java
@@ -0,0 +1,13 @@
+package Behavioral.Chain_Of_Responsibility;
+
+public class FileLogger extends AbstractLogger {
+
+    public FileLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+}
