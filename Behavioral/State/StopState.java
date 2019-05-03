1099fb70ab6c2f8e1f380a78e43e3cbe67ba3820
brijshah27 <brijshah27@gmail.com>
2019-05-03 18:38:30 -0500

    State pattern

---
+++ Behavioral/State/StopState.java
@@ -0,0 +1,13 @@
+package Behavioral.State;
+
+public class StopState implements State {
+    @Override
+    public void doAction(Context context) {
+        System.out.println("in stop state");
+        context.setState(this);
+    }
+
+    public String toString(){
+        return "Start State";
+    }
+}
