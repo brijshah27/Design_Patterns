1a804c6080f1b7d3dbbed1c49f6f7e771fc19f4d
Brij Shah <brijshah27@gmail.com>
2019-05-03 18:39:26 -0500

    State pattern

---
+++ Behavioral/State/StartState.java
@@ -0,0 +1,13 @@
+package Behavioral.State;
+
+public class StartState implements State {
+    @Override
+    public void doAction(Context context) {
+        System.out.println("in start state");
+        context.setState(this);
+    }
+
+    public String toString(){
+        return "Start State";
+    }
+}
