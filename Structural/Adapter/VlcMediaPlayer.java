be567278c38eacc47688a7a0b1eef9d557b13792
brijshah27 <brijshah27@gmail.com>
2019-05-13 18:56:54 -0500

    adapter pattern

---
+++ Structural/Adapter/VlcMediaPlayer.java
@@ -0,0 +1,13 @@
+package Structural.Adapter;
+
+public class VlcMediaPlayer implements NewMediaPlayer {
+    @Override
+    public void playVlc(String fileName) {
+        System.out.println("Playing vlc file. Name: "+ fileName);
+    }
+
+    @Override
+    public void playMp4(String fileName) {
+        //do nothing
+    }
+}
